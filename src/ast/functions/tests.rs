9f9a859805636334aa8731a710b8f77e8045f764
oleg.aysfeld <oleg.aysfeld@eltex.loc>
2025-01-16 15:59:38 +0700

    functions: add serializer test #58

--- src/ast/functions/tests.rs
+++ src/ast/functions/tests.rs
@@ -1,22 +1,64 @@
 use super::FunctionDef;
 use crate::ast::{expressions::ExpressionType, Ast};
 use crate::parser::{lexer::Lexer, token::Lexem, Parser};
+use crate::serializer::XmlWriter;
+
+#[test]
+fn function_def_test() {
+    const SRC_TEXT: &str = "\nfunc foo(a: i32, b: i32) -> f32 {\
+                             \n    return a + b\
+                             \n}";
+
+    let mut parser = Parser::new(Lexer::from_text(SRC_TEXT, true).expect("Lexer creation failed"));
+
+    let node = FunctionDef::parse(&mut parser).unwrap();
+
+    {
+        const EXPECTED: &str = "\n<function-definition name=\"foo\">\
+                                \n    <return-type>\
+                                \n        <type style=\"primitive\" name=\"f32\"/>\
+                                \n    </return-type>\
+                                \n    <parameters>\
+                                \n        <variable-definition name=\"a\" is-global=\"false\" is-mutable=\"true\">\
+                                \n            <type style=\"primitive\" name=\"i32\"/>\
+                                \n        </variable-definition>\
+                                \n        <variable-definition name=\"b\" is-global=\"false\" is-mutable=\"true\">\
+                                \n            <type style=\"primitive\" name=\"i32\"/>\
+                                \n        </variable-definition>\
+                                \n    </parameters>\
+                                \n    <return-statement>\
+                                \n        <operation style=\"binary\" operation=\"+\">\
+                                \n            <variable name=\"a\"/>\
+                                \n            <variable name=\"b\"/>\
+                                \n        </operation>\
+                                \n    </return-statement>\
+                                \n</function-definition>";
+
+        let mut buffer = Vec::<u8>::new();
+        let mut writer = XmlWriter::new(&mut buffer).unwrap();
+
+        node.serialize(&mut writer).unwrap();
+        let res = String::from_utf8(buffer).unwrap();
+
+        assert_eq!(EXPECTED, res);
+    }
+}
 
 #[test]
 fn functions_test() {
-    static SRC_TEXT: &str = "func f(a: i32, b: i32) -> f32 {\
-                                return a + b\
-                            }\
-                            func main() {\
-                                let res = f(a: 1, 2, c: 1 + 2)\
-                            }\
-                            func bar () {\
-                                let PI = 3.14\
-                            }";
+    static SRC_TEXT: &str = "\nfunc f(a: i32, b: i32) -> f32 {\
+                             \n    return a + b\
+                             \n}\
+                             \n\
+                             \nfunc main() {\
+                             \n   let res = f(a: 1, 2, c: 1 + 2)\
+                             \n}\
+                             \n\
+                             \nfunc bar () {\
+                             \n   let PI = 3.14\
+                             \n}";
 
-    let lexer = Lexer::from_text(SRC_TEXT, true).unwrap();
-
-    let mut parser = Parser::new(lexer);
+    let mut parser = Parser::new(Lexer::from_text(SRC_TEXT, true).expect("Lexer creation failed"));
 
     {
         let func = FunctionDef::parse(&mut parser).unwrap();
