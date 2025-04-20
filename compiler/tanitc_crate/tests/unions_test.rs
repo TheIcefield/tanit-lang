e6cc6c2de28df57bf3b487678bf7d333008d3a9f
oleg.icefield <oleg.icefield@gmail.com>
2025-04-20 12:34:27 +0700

    analyzer, crate: add accessing unions #5

--- compiler/tanitc_crate/tests/unions_test.rs
+++ compiler/tanitc_crate/tests/unions_test.rs
@@ -94,3 +94,99 @@ fn union_work_test() {
         assert_eq!(SOURCE_EXPECTED, source_res);
     }
 }
+
+#[test]
+fn union_in_module_work_test() {
+    const SRC_TEXT: &str = "\nmodule mod {\
+                            \n    union MyUnion {\
+                            \n        x: i32\
+                            \n        y: f32\
+                            \n    }\
+                            \n}\
+                            \nfunc main() {\
+                            \n    var u = mod::MyUnion { \
+                            \n                  y: 2.0\
+                            \n              }\
+                            \n}";
+
+    let mut parser = Parser::new(Lexer::from_text(SRC_TEXT).expect("Failed to create lexer"));
+
+    let mut program = parser.parse_global_block().unwrap();
+    {
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+    }
+
+    {
+        let mut analyzer = Analyzer::new();
+        program.accept_mut(&mut analyzer).unwrap();
+        if analyzer.has_errors() {
+            panic!("{:?}", analyzer.get_errors());
+        }
+    }
+
+    {
+        const EXPECTED: &str = "\n<module-definition name=\"mod\">\
+                                \n    <union-definition name=\"MyUnion\">\
+                                \n        <field name=\"x\">\
+                                \n            <type style=\"primitive\" name=\"i32\"/>\
+                                \n        </field>\
+                                \n        <field name=\"y\">\
+                                \n            <type style=\"primitive\" name=\"f32\"/>\
+                                \n        </field>\
+                                \n    </union-definition>\
+                                \n</module-definition>\
+                                \n<function-definition name=\"main\">\
+                                \n    <return-type>\
+                                \n        <type style=\"tuple\"/>\
+                                \n    </return-type>\
+                                \n    <operation style=\"binary\" operation=\"=\">\
+                                \n        <variable-definition name=\"u\" is-global=\"false\" is-mutable=\"false\">\
+                                \n            <type style=\"named\" name=\"MyUnion\"/>\
+                                \n        </variable-definition>\
+                                \n        <operation>\
+                                \n            <union-initialization name=\"MyUnion\">\
+                                \n                <field name=\"y\">\
+                                \n                    <literal style=\"decimal-number\" value=\"2\"/>\
+                                \n                </field>\
+                                \n            </union-initialization>\
+                                \n        </operation>\
+                                \n    </operation>\
+                                \n</function-definition>";
+
+        let mut buffer = Vec::<u8>::new();
+        let mut writer = XmlWriter::new(&mut buffer).unwrap();
+
+        program.accept(&mut writer).unwrap();
+        let res = String::from_utf8(buffer).unwrap();
+
+        assert_eq!(EXPECTED, res);
+    }
+
+    {
+        const HEADER_EXPECTED: &str = "typedef union {\
+                                     \nsigned int x;\
+                                     \nfloat y;\
+                                     \n} MyUnion;\
+                                     \nvoid main();\n";
+
+        const SOURCE_EXPECTED: &str = "void main(){\
+                                     \nMyUnion const u = (union MyUnion){\
+                                     \n.y=2,\
+                                     \n};\
+                                     \n}\n";
+
+        let mut header_buffer = Vec::<u8>::new();
+        let mut source_buffer = Vec::<u8>::new();
+        let mut writer = CodeGenStream::new(&mut header_buffer, &mut source_buffer).unwrap();
+
+        program.accept(&mut writer).unwrap();
+
+        let header_res = String::from_utf8(header_buffer).unwrap();
+        let source_res = String::from_utf8(source_buffer).unwrap();
+
+        assert_eq!(HEADER_EXPECTED, header_res);
+        assert_eq!(SOURCE_EXPECTED, source_res);
+    }
+}
