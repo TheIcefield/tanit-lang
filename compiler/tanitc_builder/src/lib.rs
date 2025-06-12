029e9f18e5a22df413bccf7fb2e823bc6f572f46
oleg.icefield <oleg.icefield@gmail.com>
2025-06-12 20:38:12 +0700

    builder: display commands errors #94

--- compiler/tanitc_builder/src/lib.rs
+++ compiler/tanitc_builder/src/lib.rs
@@ -19,73 +19,55 @@ pub fn build_object_file(
 ) -> Result<(), String> {
     let utility = get_utility_name(options.backend);
 
-    let res = Command::new(utility)
-        .arg("-c")
-        .arg(if options.crate_type == CrateType::DynamicLib {
-            "-fPIC"
-        } else {
-            ""
-        })
-        .arg(path)
-        .arg("-o")
-        .arg(output)
-        .output();
+    let mut cmd = Command::new(utility);
+    cmd.arg("-c");
 
-    if let Err(err) = res {
-        return Err(format!("Error: {err}"));
-    }
+    cmd.arg(if options.crate_type == CrateType::DynamicLib {
+        "-fPIC"
+    } else {
+        ""
+    });
 
-    Ok(())
+    cmd.arg(path);
+    cmd.arg("-o");
+    cmd.arg(output);
+
+    execute_command(&mut cmd)
 }
 
 fn build_executable(inputs: &[PathBuf], options: &CompileOptions) -> Result<(), String> {
     let utility = get_utility_name(options.backend);
 
-    let res = Command::new(utility)
-        .args(inputs)
-        .arg("-o")
-        .arg(&options.output_file)
-        .output();
+    let mut cmd = Command::new(utility);
+    cmd.args(inputs);
+    cmd.arg("-o");
+    cmd.arg(&options.output_file);
 
-    if let Err(err) = res {
-        return Err(format!("Error: {err}"));
-    }
-
-    Ok(())
+    execute_command(&mut cmd)
 }
 
 fn build_static_lib(inputs: &[PathBuf], options: &CompileOptions) -> Result<(), String> {
     const UTILITY: &str = "ar";
     const OPTIONS: &str = "rcs";
 
-    let res = Command::new(UTILITY)
-        .arg(OPTIONS)
-        .arg(&options.output_file)
-        .args(inputs)
-        .output();
+    let mut cmd = Command::new(UTILITY);
+    cmd.arg(OPTIONS);
+    cmd.arg(&options.output_file);
+    cmd.args(inputs);
 
-    if let Err(err) = res {
-        return Err(format!("Error: {err}"));
-    }
-
-    Ok(())
+    execute_command(&mut cmd)
 }
 
 fn build_dynamic_lib(inputs: &[PathBuf], options: &CompileOptions) -> Result<(), String> {
     let utility = get_utility_name(options.backend);
 
-    let res = Command::new(utility)
-        .args(inputs)
-        .arg("-shared")
-        .arg("-o")
-        .arg(&options.output_file)
-        .output();
+    let mut cmd = Command::new(utility);
+    cmd.args(inputs);
+    cmd.arg("-shared");
+    cmd.arg("-o");
+    cmd.arg(&options.output_file);
 
-    if let Err(err) = res {
-        return Err(format!("Error: {err}"));
-    }
-
-    Ok(())
+    execute_command(&mut cmd)
 }
 
 pub fn link_crate_objects(inputs: &[PathBuf], options: &CompileOptions) -> Result<(), String> {
@@ -93,5 +75,18 @@ pub fn link_crate_objects(inputs: &[PathBuf], options: &CompileOptions) -> Resul
         CrateType::Bin => build_executable(inputs, options),
         CrateType::StaticLib => build_static_lib(inputs, options),
         CrateType::DynamicLib => build_dynamic_lib(inputs, options),
+    }
+}
+
+fn execute_command(cmd: &mut Command) -> Result<(), String> {
+    match cmd.output() {
+        Ok(out) => {
+            if !out.status.success() {
+                Err(String::from_utf8(out.stderr).unwrap())
+            } else {
+                Ok(())
+            }
+        }
+        Err(err) => Err(format!("Error: {err}")),
     }
 }
