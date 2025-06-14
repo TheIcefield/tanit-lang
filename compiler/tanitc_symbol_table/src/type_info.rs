fa2c829effb3ea6c918a844f8370b764e72032e0
oleg.icefield <oleg.icefield@gmail.com>
2025-06-14 15:38:14 +0700

    compiler: add complex member access #49

--- compiler/tanitc_symbol_table/src/type_info.rs
+++ compiler/tanitc_symbol_table/src/type_info.rs
@@ -3,11 +3,14 @@ use std::collections::BTreeMap;
 use tanitc_ident::Ident;
 use tanitc_ty::Type;
 
+#[derive(Debug, Clone)]
 pub struct MemberInfo {
     pub is_public: bool,
     pub ty: Type,
 }
 
+#[derive(Debug, Clone)]
 pub struct TypeInfo {
+    pub ty: Type,
     pub members: BTreeMap<Ident, MemberInfo>,
 }
