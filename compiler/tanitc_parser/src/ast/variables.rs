31830993c70ed8c7482ee0167a9f5e37f88d0c66
oleg.aysfeld <oleg.aysfeld@eltex.loc>
2025-08-01 17:20:41 +0700

    parser,crate: add variables parser tests #156

--- compiler/tanitc_parser/src/ast/variables.rs
+++ compiler/tanitc_parser/src/ast/variables.rs
@@ -82,7 +82,7 @@ impl Parser {
             return Err(Message::from_string(
                 location,
                 format!(
-                    "Variable {identifier} defined without type, but marked as static. Need to specify type"
+                    "Variable \"{identifier}\" defined without type, but marked as static. Need to specify type"
                 ),
             ));
         }
@@ -108,5 +108,345 @@ impl Parser {
         }
 
         Ok(var_node)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tanitc_ast::{
+        expression_utils::BinaryOperation, Ast, Expression, ExpressionKind, Value, ValueKind,
+    };
+    use tanitc_ty::Type;
+
+    use crate::Parser;
+
+    #[test]
+    fn notified_vardef_test() {
+        const SRC_TEXT: &str = "var a: i32 = 0";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let res = parser.parse_variable_def().unwrap();
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+
+        let Ast::Expression(Expression {
+            kind:
+                ExpressionKind::Binary {
+                    operation,
+                    lhs,
+                    rhs,
+                },
+            ..
+        }) = &res
+        else {
+            panic!("Expected Binary expression, actually: {}", res.name());
+        };
+
+        let Ast::VariableDef(var_def) = lhs.as_ref() else {
+            panic!("Expected VariableDef, actually: {}", lhs.name());
+        };
+
+        let Ast::Value(Value {
+            kind: ValueKind::Integer(0),
+            ..
+        }) = rhs.as_ref()
+        else {
+            panic!("Expected Integer, actually: {}", rhs.name());
+        };
+
+        assert_eq!(*operation, BinaryOperation::Assign);
+
+        assert_eq!(var_def.identifier.to_string(), "a");
+        assert_eq!(var_def.mutability.is_mutable(), false);
+        assert_eq!(var_def.is_global, false);
+        assert_eq!(var_def.var_type.get_type(), Type::I32);
+    }
+
+    #[test]
+    fn notified_mut_vardef_test() {
+        const SRC_TEXT: &str = "var mut b: i32 = 0";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let res = parser.parse_variable_def().unwrap();
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+
+        let Ast::Expression(Expression {
+            kind:
+                ExpressionKind::Binary {
+                    operation,
+                    lhs,
+                    rhs,
+                },
+            ..
+        }) = &res
+        else {
+            panic!("Expected Binary expression, actually: {}", res.name());
+        };
+
+        let Ast::VariableDef(var_def) = lhs.as_ref() else {
+            panic!("Expected VariableDef, actually: {}", lhs.name());
+        };
+
+        let Ast::Value(Value {
+            kind: ValueKind::Integer(0),
+            ..
+        }) = rhs.as_ref()
+        else {
+            panic!("Expected Integer, actually: {}", rhs.name());
+        };
+
+        assert_eq!(*operation, BinaryOperation::Assign);
+
+        assert_eq!(var_def.identifier.to_string(), "b");
+        assert_eq!(var_def.mutability.is_mutable(), true);
+        assert_eq!(var_def.is_global, false);
+        assert_eq!(var_def.var_type.get_type(), Type::I32);
+    }
+
+    #[test]
+    fn unnotified_vardef_test() {
+        const SRC_TEXT: &str = "var c = 0";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let res = parser.parse_variable_def().unwrap();
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+
+        let Ast::Expression(Expression {
+            kind:
+                ExpressionKind::Binary {
+                    operation,
+                    lhs,
+                    rhs,
+                },
+            ..
+        }) = &res
+        else {
+            panic!("Expected Binary expression, actually: {}", res.name());
+        };
+
+        let Ast::VariableDef(var_def) = lhs.as_ref() else {
+            panic!("Expected VariableDef, actually: {}", lhs.name());
+        };
+
+        let Ast::Value(Value {
+            kind: ValueKind::Integer(0),
+            ..
+        }) = rhs.as_ref()
+        else {
+            panic!("Expected Integer, actually: {}", rhs.name());
+        };
+
+        assert_eq!(*operation, BinaryOperation::Assign);
+
+        assert_eq!(var_def.identifier.to_string(), "c");
+        assert_eq!(var_def.mutability.is_mutable(), false);
+        assert_eq!(var_def.is_global, false);
+        assert_eq!(var_def.var_type.get_type(), Type::Auto);
+    }
+
+    #[test]
+    fn unnotified_mut_vardef_test() {
+        const SRC_TEXT: &str = "var mut d = 0";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let res = parser.parse_variable_def().unwrap();
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+
+        let Ast::Expression(Expression {
+            kind:
+                ExpressionKind::Binary {
+                    operation,
+                    lhs,
+                    rhs,
+                },
+            ..
+        }) = &res
+        else {
+            panic!("Expected Binary expression, actually: {}", res.name());
+        };
+
+        let Ast::VariableDef(var_def) = lhs.as_ref() else {
+            panic!("Expected VariableDef, actually: {}", lhs.name());
+        };
+
+        let Ast::Value(Value {
+            kind: ValueKind::Integer(0),
+            ..
+        }) = rhs.as_ref()
+        else {
+            panic!("Expected Integer, actually: {}", rhs.name());
+        };
+
+        assert_eq!(*operation, BinaryOperation::Assign);
+
+        assert_eq!(var_def.identifier.to_string(), "d");
+        assert_eq!(var_def.mutability.is_mutable(), true);
+        assert_eq!(var_def.is_global, false);
+        assert_eq!(var_def.var_type.get_type(), Type::Auto);
+    }
+
+    #[test]
+    fn notified_vardec_test() {
+        const SRC_TEXT: &str = "var e: i32";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let res = parser.parse_variable_def().unwrap();
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+
+        let Ast::VariableDef(var_def) = &res else {
+            panic!("Expected VariableDef, actually: {}", res.name());
+        };
+
+        assert_eq!(var_def.identifier.to_string(), "e");
+        assert_eq!(var_def.mutability.is_mutable(), false);
+        assert_eq!(var_def.is_global, false);
+        assert_eq!(var_def.var_type.get_type(), Type::I32);
+    }
+
+    #[test]
+    fn notified_mut_vardec_test() {
+        const SRC_TEXT: &str = "var mut f: f32";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let res = parser.parse_variable_def().unwrap();
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+
+        let Ast::VariableDef(var_def) = &res else {
+            panic!("Expected VariableDef, actually: {}", res.name());
+        };
+
+        assert_eq!(var_def.identifier.to_string(), "f");
+        assert_eq!(var_def.mutability.is_mutable(), true);
+        assert_eq!(var_def.is_global, false);
+        assert_eq!(var_def.var_type.get_type(), Type::F32);
+    }
+
+    #[test]
+    fn unnotified_vardec_bad_test() {
+        const SRC_TEXT: &str = "var e";
+        const EXPECTED_ERR: &str =
+            "Variable \"e\" defined without type. Need to specify type or use with rvalue";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let msg = parser.parse_variable_def().err().unwrap();
+        assert_eq!(msg.text, EXPECTED_ERR);
+    }
+
+    #[test]
+    fn notified_static_def_test() {
+        const SRC_TEXT: &str = "static A: i32 = 0";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let res = parser.parse_variable_def().unwrap();
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+
+        let Ast::Expression(Expression {
+            kind:
+                ExpressionKind::Binary {
+                    operation,
+                    lhs,
+                    rhs,
+                },
+            ..
+        }) = &res
+        else {
+            panic!("Expected Binary expression, actually: {}", res.name());
+        };
+
+        let Ast::VariableDef(var_def) = lhs.as_ref() else {
+            panic!("Expected VariableDef, actually: {}", lhs.name());
+        };
+
+        let Ast::Value(Value {
+            kind: ValueKind::Integer(0),
+            ..
+        }) = rhs.as_ref()
+        else {
+            panic!("Expected Integer, actually: {}", rhs.name());
+        };
+
+        assert_eq!(*operation, BinaryOperation::Assign);
+
+        assert_eq!(var_def.identifier.to_string(), "A");
+        assert_eq!(var_def.mutability.is_mutable(), false);
+        assert_eq!(var_def.is_global, true);
+        assert_eq!(var_def.var_type.get_type(), Type::I32);
+    }
+
+    #[test]
+    fn notified_static_mut_def_test() {
+        const SRC_TEXT: &str = "static mut B: i32 = 0";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let res = parser.parse_variable_def().unwrap();
+        if parser.has_errors() {
+            panic!("{:?}", parser.get_errors());
+        }
+
+        let Ast::Expression(Expression {
+            kind:
+                ExpressionKind::Binary {
+                    operation,
+                    lhs,
+                    rhs,
+                },
+            ..
+        }) = &res
+        else {
+            panic!("Expected Binary expression, actually: {}", res.name());
+        };
+
+        let Ast::VariableDef(var_def) = lhs.as_ref() else {
+            panic!("Expected VariableDef, actually: {}", lhs.name());
+        };
+
+        let Ast::Value(Value {
+            kind: ValueKind::Integer(0),
+            ..
+        }) = rhs.as_ref()
+        else {
+            panic!("Expected Integer, actually: {}", rhs.name());
+        };
+
+        assert_eq!(*operation, BinaryOperation::Assign);
+
+        assert_eq!(var_def.identifier.to_string(), "B");
+        assert_eq!(var_def.mutability.is_mutable(), true);
+        assert_eq!(var_def.is_global, true);
+        assert_eq!(var_def.var_type.get_type(), Type::I32);
+    }
+
+    #[test]
+    fn unnotified_static_def_bad_test() {
+        const SRC_TEXT: &str = "static C = 0";
+        const EXPECTED_ERR: &str =
+            "Variable \"C\" defined without type, but marked as static. Need to specify type";
+
+        let mut parser = Parser::from_text(SRC_TEXT).unwrap();
+
+        let msg = parser.parse_variable_def().err().unwrap();
+        assert_eq!(msg.text, EXPECTED_ERR);
     }
 }
