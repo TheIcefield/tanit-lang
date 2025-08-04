0dcb288d6d5b8bdc58aa3f7b12b2f2f2814c8868
oleg.aysfeld <oleg.icefield@gmail.com>
2025-08-04 21:59:42 +0700

    codegen: add extern test #142

--- compiler/tanitc_codegen/src/c_generator/ast/externs.rs
+++ compiler/tanitc_codegen/src/c_generator/ast/externs.rs
@@ -16,3 +16,45 @@ impl CodeGenStream<'_> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use tanitc_ast::ast::{externs::ExternDef, functions::FunctionDef, types::TypeSpec, Ast};
+    use tanitc_ident::Ident;
+    use tanitc_ty::Type;
+
+    use pretty_assertions::assert_str_eq;
+
+    use crate::c_generator::CodeGenStream;
+
+    #[test]
+    fn extern_test() {
+        const HEADER_EXPECTED: &str = "signed int c_func();\n";
+
+        let extern_node = Ast::from(ExternDef {
+            abi_name: "C".to_string(),
+            functions: vec![FunctionDef {
+                identifier: Ident::from("c_func".to_string()),
+                parameters: vec![],
+                return_type: TypeSpec {
+                    ty: Type::I32,
+                    ..Default::default()
+                },
+                ..Default::default()
+            }],
+            ..Default::default()
+        });
+
+        let mut header_buffer = Vec::<u8>::new();
+        let mut source_buffer = Vec::<u8>::new();
+        let mut writer = CodeGenStream::new(&mut header_buffer, &mut source_buffer).unwrap();
+
+        extern_node.accept(&mut writer).unwrap();
+
+        let header_res = String::from_utf8(header_buffer).unwrap();
+        assert_str_eq!(header_res, HEADER_EXPECTED);
+
+        let source_res = String::from_utf8(source_buffer).unwrap();
+        assert!(source_res.is_empty());
+    }
+}
