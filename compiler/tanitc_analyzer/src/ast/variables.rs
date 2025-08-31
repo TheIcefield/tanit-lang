0ac74e136bf104ecd00ce78ae7ab4427ac7c3ec5
oleg.aysfeld <oleg.icefield@gmail.com>
2025-08-31 13:37:49 +0700

    analyzer: add ref to ptr coercion #164

--- compiler/tanitc_analyzer/src/ast/variables.rs
+++ compiler/tanitc_analyzer/src/ast/variables.rs
@@ -1,4 +1,5 @@
 use tanitc_ast::ast::{variables::VariableDef, Ast};
+use tanitc_lexer::location::Location;
 use tanitc_messages::Message;
 use tanitc_symbol_table::{
     entry::{Entry, SymbolKind, VarDefData, VarStorageType},
@@ -41,6 +42,77 @@ impl Analyzer {
         type_info
     }
 
+    fn check_ref_coerce_to_ptr(
+        &self,
+        src_type: &Type,
+        dst_type: &Type,
+        location: Location,
+    ) -> Result<(), Message> {
+        let Type::Ref { ref_to, .. } = src_type else {
+            return Err(Message::unreachable(
+                location,
+                format!("dst_type expected to be reference, actually: {src_type}"),
+            ));
+        };
+
+        let Type::Ptr(ptr_to) = dst_type else {
+            return Err(Message::unreachable(
+                location,
+                format!("src_type expected to be pointer, actually: {dst_type}"),
+            ));
+        };
+
+        self.compare_types(ref_to, ptr_to.as_ref(), location)
+    }
+
+    // Returns true if src_type can be coerced to dst_type, otherwise - false
+    fn try_coerce(&self, src_type: &Type, dst_type: &Type, location: Location) -> bool {
+        if src_type.is_reference() && dst_type.is_pointer() {
+            return self
+                .check_ref_coerce_to_ptr(src_type, dst_type, location)
+                .is_ok();
+        }
+
+        false
+    }
+
+    fn compare_types(
+        &self,
+        lhs_type: &Type,
+        rhs_type: &Type,
+        location: Location,
+    ) -> Result<(), Message> {
+        let mut alias_to = self.find_alias_value(lhs_type);
+
+        if lhs_type == rhs_type {
+            alias_to = None;
+        }
+
+        if alias_to.is_none()
+            && lhs_type != rhs_type
+            && !self.try_coerce(rhs_type, lhs_type, location)
+        {
+            return Err(Message {
+                    location,
+                    text: format!(
+                        "Cannot perform operation on objects with different types: {lhs_type} and {rhs_type}",
+                    ),
+                });
+        } else if alias_to.as_ref().is_some_and(|ty| rhs_type != ty)
+            && !self.try_coerce(rhs_type, lhs_type, location)
+        {
+            return Err(Message {
+                    location,
+                    text: format!(
+                        "Cannot perform operation on objects with different types: {lhs_type} (aka: {}) and {rhs_type}",
+                        alias_to.unwrap()
+                    ),
+                });
+        }
+
+        Ok(())
+    }
+
     pub fn check_variable_def_types(
         &mut self,
         lhs: &mut VariableDef,
@@ -66,36 +138,14 @@ impl Analyzer {
         }
 
         let var_type = lhs.var_type.get_type();
-
-        let mut alias_to = self.find_alias_value(&var_type);
-
-        if var_type == rhs_type.ty {
-            alias_to = None;
-        }
-
-        if alias_to.is_none() && var_type != rhs_type.ty {
-            return Err(Message {
-                    location: lhs.location,
-                    text: format!(
-                        "Cannot perform operation on objects with different types: {var_type} and {rhs_type}",
-                    ),
-                });
-        } else if alias_to.as_ref().is_some_and(|ty| rhs_type.ty != *ty) {
-            return Err(Message {
-                    location: lhs.location,
-                    text: format!(
-                        "Cannot perform operation on objects with different types: {var_type} (aka: {}) and {rhs_type}",
-                        alias_to.unwrap()
-                    ),
-                });
-        }
+        self.compare_types(&var_type, &rhs_type.ty, lhs.location)?;
 
         self.add_symbol(Entry {
             name: variable_name,
             is_static: false,
             kind: SymbolKind::from(VarDefData {
                 storage: VarStorageType::Auto,
-                var_type: lhs.var_type.get_type(),
+                var_type,
                 mutability: lhs.mutability,
                 is_initialization: true,
             }),
